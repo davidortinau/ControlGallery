654c7d6bf5eac3e9b3248384f3036d1146d6e7f5
David Ortinau <david.ortinau@microsoft.com>
2021-03-10 07:48:41 -0600

    initial commit

---
+++ src/ControlGallery/ControlGallery/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ControlGallery
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ src/ControlGallery/ControlGallery/Pages/LabelPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControlGallery.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabelPage : ContentPage
+    {
+        public LabelPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
