374e857afc2edf2aa5760436f53a584e15fb6f3c
davidortinau <david.ortinau@microsoft.com>
2021-06-16 09:32:25 -0500

    added shell, more pages

---
+++ src/ControlGallery/Pages/ImagePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace ControlGallery.Pages
+{
+    public partial class ImagePage : ContentPage
+    {
+        public ImagePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
