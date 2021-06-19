999f89e385df7e5b37928fe6879228c3b0d3d8c8
David Ortinau <david.ortinau@microsoft.com>
2021-06-19 12:34:59 -0500

    added more label styles, and apptheme colors

---
+++ src/ControlGallery/Pages/AppThemePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace ControlGallery.Pages
+{
+    public partial class AppThemePage : ContentPage
+    {
+        public AppThemePage()
+        {
+            InitializeComponent();
+        }
+
+        void OnChangeTheme(object s, EventArgs e)
+        {
+            OSAppTheme currentTheme = Application.Current.RequestedTheme;
+            Application.Current.UserAppTheme = (currentTheme == OSAppTheme.Light) ? OSAppTheme.Dark : OSAppTheme.Light;
+            CurrentThemeLabel.Text = Application.Current.UserAppTheme.ToString();
+        }
+
+    }
+}
