cd31e0eba822de374a58816d36acbfef3ae5503c
David Ortinau <david.ortinau@microsoft.com>
2021-07-29 22:54:08 -0500

    testing layouts with pre7

--- src/ControlGallery/Pages/Stack/AlignmentStackPage.cs
+++ src/ControlGallery/Pages/Stack/AlignmentStackPage.cs
@@ -8,16 +8,20 @@ namespace LayoutLab
 	{
 		public AlignmentStackPage()
 		{
+			Padding = new Thickness(0);
 			BackgroundColor = Colors.White;
-			this.Content = new StackLayout
-			{
-				Margin = new Thickness(20),
-				Children =
-                {
-					new Label { Text = "Start", BackgroundColor = Colors.Gray, HorizontalOptions = LayoutOptions.Start },
-					new Label { Text = "Center", BackgroundColor = Colors.Gray, HorizontalOptions = LayoutOptions.Center },
-					new Label { Text = "End", BackgroundColor = Colors.Gray, HorizontalOptions = LayoutOptions.End },
-					new Label { Text = "Fill", BackgroundColor = Colors.Gray, HorizontalOptions = LayoutOptions.Fill }
+
+			this.Content = new ScrollView(){
+				Content = new StackLayout
+				{
+					Margin = new Thickness(20),
+					Children =
+					{
+						new Label { Text = "Start", BackgroundColor = Colors.Gray, HorizontalOptions = LayoutOptions.Start, Padding = new Thickness(10, 8) },
+						new Label { Text = "Center", BackgroundColor = Colors.Gray, HorizontalOptions = LayoutOptions.Center, Padding = new Thickness(10, 8) },
+						new Label { Text = "End", BackgroundColor = Colors.Gray, HorizontalOptions = LayoutOptions.End, Padding = new Thickness(10, 8) },
+						new Label { Text = "Fill", BackgroundColor = Colors.Gray, HorizontalOptions = LayoutOptions.Fill, Padding = new Thickness(10, 8) }
+					}
 				}
 			};
 		}
