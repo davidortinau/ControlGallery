774c87acb6d8795a48af106fdc5eb7238b61e8cf
David Ortinau <david.ortinau@microsoft.com>
2022-05-07 15:54:05 -0500

    styling for light/dark and updated colors

--- src/ControlGallery/MauiProgram.cs
+++ src/ControlGallery/MauiProgram.cs
@@ -1,3 +1,10 @@
+#if WINDOWS
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
 namespace ControlGallery;
 
 public static class MauiProgram
@@ -12,8 +19,30 @@ public static class MauiProgram
 				fonts.AddFont("fa_solid.ttf", "FontAwesome");
 				fonts.AddFont("opensans_regular.ttf", "OpenSansRegular");
 				fonts.AddFont("opensans_semibold.ttf", "OpenSansSemiBold");
-			}); 
+			});
 
-		return builder.Build();
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(wndLifeCycleBuilder =>
+                {
+                    wndLifeCycleBuilder.OnWindowCreated(window =>
+                    {
+                        IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                        AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);
+
+                        const int width = 1200;
+                        const int height = 800;
+                        int x = 1920 / 2 - width / 2; //Convert.ToInt32(DeviceDisplay.MainDisplayInfo.Width)
+                        int y = 1080 / 2 - height / 2; //Convert.ToInt32(DeviceDisplay.MainDisplayInfo.Height)
+
+                        winuiAppWindow.MoveAndResize(new RectInt32(x, y, width, height));
+                    });
+                });
+            });
+#endif
+
+        return builder.Build();
 	}
 }
