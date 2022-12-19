2c4253e9e9cd0f077ad5e996603f7ab87d49f040
David Ortinau <david.ortinau@microsoft.com>
2022-12-19 13:04:10 -0600

    display maui blogs and navigate to browser

---
+++ src/ControlGallery/Models/QuickLink.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlGallery.Models
+{
+    public class QuickLink
+    {
+        public string FeaturedImage { get; set; }
+        public string Hyperlink { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Title { get; set; }
+
+        public Command NavigateTo { private set; get; }
+
+        public QuickLink()
+        {
+            NavigateTo = new Command(
+                async () =>
+                {
+                    try{
+                        await Browser.OpenAsync(Hyperlink);
+                    }catch(Exception ex){
+                        Debug.WriteLine($"Crap: {ex.Message}");
+                    }
+                });
+        }
+    }
+}
---
+++ src/ControlGallery/Pages/HomeViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using CodeHollow.FeedReader;
+using AsyncAwaitBestPractices;
+using System.Diagnostics;
+
+namespace ControlGallery.Pages;
+
+[INotifyPropertyChanged]
+public partial class HomeViewModel
+{
+    [ObservableProperty]
+    private List<FeedItem> articles;
+
+    [RelayCommand]
+    async Task NavigateTo(string link)
+    {
+        try
+        {
+            await Browser.OpenAsync(link);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Crap: {ex.Message}");
+        }
+    }
+
+	public HomeViewModel()
+    {
+		LoadBlogs().SafeFireAndForget();
+	}
+
+    async Task LoadBlogs()
+    {
+        var feed = await FeedReader.ReadAsync("https://devblogs.microsoft.com/dotnet/category/maui/feed/");
+
+        //Console.WriteLine("Feed Title: " + feed.Title);
+        //Console.WriteLine("Feed Description: " + feed.Description);
+        //Console.WriteLine("Feed Image: " + feed.ImageUrl);
+        //// ...
+        //foreach (var item in feed.Items)
+        //{
+        //    Console.WriteLine(item.Title + " - " + item.Link);
+        //}
+        try
+        {
+            Articles = new List<FeedItem>(feed.Items);
+        }catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+
+
+}
+
+
+
