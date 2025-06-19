ac55cdffe7e25038e936cfca8f9e2c39e8a0a3ad
David Ortinau <david.ortinau@microsoft.com>
2025-06-18 21:05:44 -0500

    refactor: clean up XAML namespaces and improve code consistency across pages

--- src/ControlGallery/_xmlns.cs
+++ src/ControlGallery/_xmlns.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using XmlnsPrefixAttribute = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
 
 [assembly: XmlnsDefinition(
@@ -12,5 +13,43 @@ using XmlnsPrefixAttribute = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
 [assembly: XmlnsDefinition(
     "http://schemas.microsoft.com/dotnet/maui/global",
     "ControlGallery.Common.Behaviors")]    
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "http://schemas.microsoft.com/dotnet/2022/maui/toolkit")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "http://schemas.syncfusion.com/maui/toolkit")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "ControlGallery.Models")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "CustomLayouts")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "CodeHollow.FeedReader", AssemblyName = "CodeHollow.FeedReader")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "ControlGallery.Common.Effects")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "ControlGallery.Pages.Layouts")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "ControlGallery.Pages.Layouts.AbsoluteLayouts")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "CollectionViewDemos.Views")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "ControlGallery.Pages.Features.Animation")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "Flexibility.Shared")]
 
-[assembly: XmlnsPrefix("ControlGallery.Common.Views","views")]
+[assembly: XmlnsPrefix("ControlGallery.Common.Views", "views")]
+[assembly: XmlnsPrefix("http://schemas.microsoft.com/dotnet/2022/maui/toolkit", "toolkit")]
+
+//  xmlns:feed="clr-namespace:CodeHollow.FeedReader;assembly=CodeHollow.FeedReader"
+             
+             
